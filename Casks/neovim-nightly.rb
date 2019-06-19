c5a605b018c05d08b90c4b794c088d25736997c3
Jason Cheatham <j.cheatham@gmail.com>
2019-06-19 09:25:19 -0400

    Initial commit

---
+++ Casks/neovim-nightly.rb
@@ -0,0 +1,10 @@
+cask 'neovim-nightly' do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  name 'Neovim Nightly'
+  homepage 'https://neovim.io/'
+
+  binary 'nvim-osx64/bin/nvim'
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# homebrew-neovim-nightly
+
+A tap for [Homebrew](https://brew.sh) that provides a formula to install the
+current pre-built
+[neovim nightly](https://github.com/neovim/neovim/releases/tag/nightly)
