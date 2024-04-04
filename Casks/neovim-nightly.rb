57840f8da0fa1e56cdc742e4fc2705ae9a0cd00a
Tim Millican <timothy.millican@walmart.com>
2024-04-04 10:46:10 -0400

    add multiarch for arm macbooks

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,8 +1,11 @@
 cask 'neovim-nightly' do
+  arch arm: "aarch64", "intel": "x86_64"
+  arch_fragment = on_arch_conditional arm: "arm64", intel: "x86_64"
+
   version :latest
   sha256 :no_check
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch_fragment}.tar.gz"
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
