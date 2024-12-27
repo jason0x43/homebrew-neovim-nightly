4e05f06d5be7bdf9f9578266f8f33320f8c1dae3
Jason Cheatham <jason@jasoncheatham.com>
2024-12-27 11:05:51 -0500

    Support ARM

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -2,9 +2,9 @@ cask 'neovim-nightly' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
-  binary 'nvim-osx64/bin/nvim'
+  binary 'nvim-macos-arm64/bin/nvim'
 end
