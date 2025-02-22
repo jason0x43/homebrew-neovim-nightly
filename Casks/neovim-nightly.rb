48db8828ffc790e344dcbebed4971e282dd8c8ba
Jason Cheatham <jason@jasoncheatham.com>
2025-02-21 21:22:36 -0700

    Fix executable being blocked on macOS

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,10 +1,19 @@
 cask 'neovim-nightly' do
+  conflicts_with formula: 'neovim'
+
   version :latest
   sha256 :no_check
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+  arch arm: 'arm64'
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+      verified: 'github.com/neovim'
+
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
-  binary 'nvim-macos-arm64/bin/nvim'
+  binary "nvim-macos-#{arch}/bin/nvim"
+
+  postflight do
+    system_command 'xattr', args: ['-cr', staged_path.to_s]
+  end
 end
